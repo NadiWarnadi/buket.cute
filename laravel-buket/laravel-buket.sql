d958a89ae42390c0222ec29ea7f79963a310c14a
NadiWarnadi <warnadi2006@gmail.com>
2026-03-14 19:15:33 +0700

    updet sql penamabahan media di chat tidak kompitable

--- laravel-buket/laravel-buket.sql
+++ laravel-buket/laravel-buket.sql
@@ -179,3 +179,6 @@ CREATE TABLE `users` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
